40160a430e9bfdae768613e700f17ead09434f6b
Jesus Colunga <jesus.colunga@proapi.com.mx>
2019-05-24 23:18:20 -0500

    Manager add to inventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -22,13 +22,24 @@ var menuOps = [{
               "Exit"],
     name:    "action"
 }];
+var q1 = [
+    {type:    "number",
+     message: "Please type the 'id' of the product you want to increment:",
+     name:    "q1Id"},
+    {type:    "number",
+     message: "How many units of the product would you like to add?",
+     name:    "q1Qty"}
+    ];
 var fs = require("fs");
 var s = "";   // String for general use.
 
 // OBJECTS
 // =======================================================================================
 var info = {
-    menuOpt        : ""
+    menuOpt        : "",
+    q1Id           : "",
+    q1Qty          : 0,
+    stock_quantity : 0
 };
 
 // Functions
@@ -42,6 +53,60 @@ function writeLog (strg) {
                     };
                   }
                  );
+};
+// - - - - - - - - - - - - - - Add to Inventory
+//If a manager selects `Add to Inventory`,
+// your app should display a prompt that will let the manager
+// "add more" of any item currently in the store.
+function addExistence(){
+    var query = connection.query(
+		"UPDATE products SET ? WHERE ?",
+		[ {stock_quantity: info.stock_quantity + info.q1Qty},
+		  {item_id: info.q1Id} ],
+        function(err, reg) {
+            if (err) throw err;
+            console.log("\nQuantity updated.\n");
+            writeLog("\nQuantity updated.\n");
+            processProducts();
+        }
+      );
+};
+
+function processExistenceValidate (reg){
+    if (reg.length <= 0) {
+        connection.end();
+        console.log("Error identifing the product Id you typed.");
+    } else {
+        info.stock_quantity = parseFloat(reg[0].stock_quantity);
+        addExistence();
+    };
+};
+
+function checkExistence(){
+    var query = connection.query(
+        "SELECT * FROM products WHERE ?",
+        {item_id: info.q1Id},
+        function(err, reg) {
+            if (err) throw err;
+            processExistenceValidate (reg);
+        }
+      );
+};
+
+function processAddInv(){
+    inquirer
+      .prompt (q1)
+      .then ( function (resp) { 
+          info.q1Id = resp.q1Id;
+          if ( !isNaN(resp.q1Qty) ) {
+            info.q1Qty = parseFloat(resp.q1Qty);
+            checkExistence(); 
+          } else {
+              console.log("Not a valid quantity.");
+              writeLog("Not a valid quantity.");
+              connection.end();
+          }
+        } );
 };
 
 // - - - - - - - - - - - - - - Common to some processes
@@ -104,7 +169,7 @@ function processProducts(){
 function checkMenuOption () {
     if (info.menuOpt === "View Products for Sale") processProducts(); else
     if (info.menuOpt === "View Low Inventory"    ) processLowInv();   else
-    if (info.menuOpt === "Add to Inventory"      ) {} else
+    if (info.menuOpt === "Add to Inventory"      ) processAddInv();   else
     if (info.menuOpt === "Add New Product"       ) {}; 
 };
 
